8bc4da02d0ddb1ed58ad7e882ea53952e780b2f0
mmpconsulting <office@mmpconsulting.net>
2026-08-04 13:35:23 +0700

    Reorder models/__init__.py to import subscription_package model before extension models

--- models/__init__.py
+++ models/__init__.py
@@ -19,14 +19,14 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
+from . import subscription_package
+from . import subscription_package_plan
+from . import subscription_package_stage
+from . import subscription_package_stop
+from . import subscription_package_product_line
 from . import account_move
 from . import product_template
 from . import recurrence_period
 from . import res_partner
 from . import sale_order
-from . import subscription_package
-from . import subscription_package_plan
-from . import subscription_package_product_line
-from . import subscription_package_stage
-from . import subscription_package_stop
 from . import sale_order_line
