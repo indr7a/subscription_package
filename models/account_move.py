446b8e050243930d7b17505d4855ef997ccb5b1d
mmpconsulting <office@mmpconsulting.net>
2026-08-04 13:17:39 +0700

    Fix singleton ValueError in compute methods and multi-record create logic

---
+++ models/account_move.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: JANISH BABU (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    """Inherited sale order model"""
+    _inherit = "account.move"
+
+    is_subscription = fields.Boolean(string='Is Subscription', default=False,
+                                     help='Is subscription')
+    subscription_id = fields.Many2one('subscription.package',
+                                      string='Subscription',
+                                      help='Choose subscription package')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """ It displays subscription in account move """
+        for rec in vals_list:
+            origin = rec.get('invoice_origin')
+            if origin:
+                so_id = self.env['sale.order'].search([('name', '=', origin)], limit=1)
+                if so_id and so_id.is_subscription:
+                    if so_id.subscription_id and so_id.subscription_id.next_invoice_date:
+                        so_id.subscription_id.start_date = so_id.subscription_id.next_invoice_date
+                    rec.update({
+                        'is_subscription': True,
+                        'subscription_id': so_id.subscription_id.id if so_id.subscription_id else False
+                    })
+        return super().create(vals_list)
